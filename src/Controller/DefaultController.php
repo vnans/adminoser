401c509e066c45eb6958990d975d126257cb5755
vnans <adelph_85@hotmail.com>
2018-12-20 15:36:20 +0000

    ajout page accueil web

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,12 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use App\Repository\ActualiteRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
 
     /**
-     * @Route("/", name="accueil")
+     * @Route("/", name="defaut")
+     */
+    public function defaut(ArticleRepository $articleRepository , ActualiteRepository $actualiteRepository ): Response
+    {
+        // return $this->render('@FOSUser/Security/login.html.twig', array(
+        //     'last_username' => null,
+        //     'error' => null,
+        //     'csrf_token' => null,
+        // ) );
+        return $this->render('default/defaut.html.twig' ,[
+            'actualites' => $actualiteRepository->findAll(),
+            'articles' => $articleRepository->findAll()
+        ]);
+    }
+/**
+     * @Route("/accueil", name="accueil")
      */
     public function accueil()
     {
---
+++ src/Entity/Article.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $ladate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getLadate(): ?\DateTimeInterface
+    {
+        return $this->ladate;
+    }
+
+    public function setLadate(\DateTimeInterface $ladate): self
+    {
+        $this->ladate = $ladate;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
